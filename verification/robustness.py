93249202f248e365d44c643e0d5e94dcccd60420
John Grese <johngrese@yahoo.com>
2020-04-28 20:38:33 -0700

    added bruteforce distance test

--- verification/robustness.py
+++ verification/robustness.py
@@ -17,8 +17,8 @@ def evaluate_sample(nnet_path, input_sample, output_sample):
             counterexamples.append(vals)
     return counterexamples
 
-def find_feature_sensitivity(nnet_path, x, samples, d_min=0.01, d_max=50.00, d_step=0.01):
-
+# TODO: Fix RecursionError stack overflow (iterative instead of recursion)
+def find_feature_sensitivity_recursive_backtrack(nnet_path, x, samples, d_min=0.01, d_max=50.00, d_step=0.01):
     def find_shortest_distance(sample, distances, curr_i=0, prev_i=0):
         input_sample, output_sample = sample
         num_dist = len(distances)
@@ -57,8 +57,29 @@ def find_feature_sensitivity(nnet_path, x, samples, d_min=0.01, d_max=50.00, d_s
         results.append((neg_d, pos_d))
     return (max(list(zip(*results))[0]), min(list(zip(*results))[1]))
 
+def find_feature_sensitivity_bruteforce(nnet_path, x, samples, d_min=0.01, d_max=100.00, d_step=0.01):
+    l_dist = 0
+    r_dist = 0
+    distances = np.round(np.arange(d_min, d_max, d_step), count_decimal_places(d_step))
+    for i,(input_sample, output_sample) in enumerate(samples):
+        for d in distances:
+            if d >= l_dist:
+                l_sample = input_sample[0:x] + [input_sample[x]-d] + input_sample[x+1:]
+                l_result = evaluate_sample(nnet_path, l_sample, output_sample)
+                if len(l_result) > 0:
+                    l_dist = d
+                    break
+        for d in distances:
+            if d >= r_dist:
+                r_sample = input_sample[0:x] + [input_sample[x]+d] + input_sample[x+1:]
+                r_result = evaluate_sample(nnet_path, r_sample, output_sample)
+                if len(r_result) > 0:
+                    r_dist = d
+                    break
+    return (l_dist, r_dist)
+
 def test_network_sensitivity(nnet_path, n_features, samples):
     results = {}
     for x in range(n_features):
-        results[f'x{x}'] = find_feature_sensitivity(nnet_path, x, samples)
+        results[f'x{x}'] = find_feature_sensitivity_bruteforce(nnet_path, x, samples)
     return results
