af7e3a1671a35cdecd06003581f681139dc8af94
John Grese <johngrese@yahoo.com>
2020-04-28 20:29:37 -0700

    totnet class for takeovertime

---
+++ verification/tot_net.py
@@ -0,0 +1,78 @@
+from maraboupy import MarabouCore
+from maraboupy.Marabou import createOptions
+import numpy as np
+
+class TOTNet:
+    '''
+    Class representing SCAD TOT Marabou network
+    '''
+    def __init__(self, network_path, property_path='', lbs=None, ubs=None):
+        self.network_path = network_path
+        self.load_query(property_path)
+        if not(lbs is None and ubs is None):
+            assert(len(lbs) == len(ubs))
+            assert(len(lbs) == self.ipq.getNumInputVariables())
+            for i in range(len(lbs)):
+                self.set_input_lower_bound(i, lbs[i])
+                self.set_input_upper_bound(i, ubs[i])
+
+    def load_query(self, property_path=''):
+        self.ipq = MarabouCore.InputQuery()
+        MarabouCore.createInputQuery(self.ipq, self.network_path, property_path)
+    
+    def set_lower_bounds(self, scaled_values):
+        assert(len(scaled_values) == self.ipq.getNumInputVariables())
+        for x,v in enumerate(scaled_values):
+            self.set_input_lower_bound(x, v)
+
+    def set_upper_bounds(self, scaled_values):
+        assert(len(scaled_values) == self.ipq.getNumInputVariables())
+        for x,v in enumerate(scaled_values):
+            self.set_input_upper_bound(x, v)
+
+    def set_input_lower_bound(self, x_index, scaled_value):
+        assert(x_index < self.ipq.getNumInputVariables())
+        variable = self.ipq.inputVariableByIndex(x_index)
+        self.ipq.setLowerBound(variable, scaled_value)
+    
+    def set_input_upper_bound(self, x_index, scaled_value):
+        assert(x_index < self.ipq.getNumInputVariables())
+        variable = self.ipq.inputVariableByIndex(x_index)
+        self.ipq.setUpperBound(variable, scaled_value)
+    
+    def set_expected_category(self, y_index):
+        n_outputs = self.ipq.getNumOutputVariables()
+        assert(y_index < n_outputs)
+        other_cats_y = [y for y in range(y_index) if y is not y_index]
+        for other_y in other_cats_y:
+            eq = MarabouCore.Equation(MarabouCore.Equation.LE)
+            eq.addAddend(1, self.ipq.outputVariableByIndex(other_y))
+            eq.addAddend(-1, self.ipq.outputVariableByIndex(y_index))
+            eq.setScalar(0)
+            self.ipq.addEquation(eq)
+    
+    def get_lower_bounds(self):
+        n = self.ipq.getNumInputVariables()
+        return [self.ipq.getLowerBound(self.ipq.inputVariableByIndex(x)) for x in range(n)]
+    
+    def get_upper_bounds(self):
+        n = self.ipq.getNumInputVariables()
+        return [self.ipq.getUpperBound(self.ipq.inputVariableByIndex(x)) for x in range(n)]
+
+    def get_bounds(self):
+        return self.get_lower_bounds(), self.get_upper_bounds()
+
+    def solve(self, output_path='', timeout=0):
+        options = createOptions(timeoutInSeconds=timeout)
+        vals, stats = MarabouCore.solve(self.ipq, options, output_path)
+        assignment = []
+        if len(vals) > 0:
+            for i in range(self.ipq.getNumInputVariables()):
+                assignment.append(f'input {i} = {vals[self.ipq.inputVariableByIndex(i)]}')
+            for i in range(self.ipq.getNumOutputVariables()):
+                assignment.append(f'output {i} = {vals[self.ipq.outputVariableByIndex(i)]}')
+        return [assignment, stats]
+    
+    def reset_query(self):
+        self.load_query()
+
