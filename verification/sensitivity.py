8767e52798a27a31899841c479ffa3d5f020182f
John Grese <johngrese@yahoo.com>
2020-05-15 13:17:33 -0700

    sensitivity results to csv & cleanup
    * add support to save sensitivity results to csv
    * added some comments and renamed a few variables

--- verification/sensitivity.py
+++ verification/sensitivity.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from tot_net import TOTNet
 from utils import create_logger, count_decimal_places, ms_since_1970
@@ -7,17 +8,51 @@ from maraboupy import Marabou, MarabouUtils, MarabouCore
 
 logger = create_logger('sensitivity')
 
-def evaluate_sample(nnet_path, input_sample, output_sample):
-    expected_cat = output_sample.index(max(output_sample))
-    other_cats = [i for i in range(len(output_sample)) if i != expected_cat]
+def evaluate_sample(nnet_path, inputs, outputs):
+    '''
+    evaluates a sample using marabou
+
+    @param nnet_path (string): path to the nnet file
+    @param inputs (list): x values for sample
+    @param outputs (list): y values for sample
+    @return (tuple): (UNSAT|SAT, [y0,...,yN])
+    '''
+    expected_cat = outputs.index(max(outputs))
+    other_cats = [i for i in range(len(outputs)) if i != expected_cat]
     net = Marabou.read_nnet(nnet_path)
-    for x,v in enumerate(input_sample):
+    for x,v in enumerate(inputs):
         net.setLowerBound(net.inputVars[0][x], v)
         net.setUpperBound(net.inputVars[0][x], v)
-    pred = net.evaluate([input_sample])
+    pred = net.evaluate([inputs])
     pred_cat = list(pred[0]).index(max(pred[0]))
     result = 'UNSAT' if pred_cat == expected_cat else 'SAT'
     return (result, pred)
+
+def save_sensitivity_results_to_csv(results, outdir='../artifacts/sensitivity', outid=None):
+    '''
+    saves sensitivity summary and detailed results in csv format.
+
+    @param results (dict): sensitivity results dictionary ({x0: (summary_tuple, details_list), ...})
+    @param outdir (string): output directory
+    @param outid (string): output file id
+    '''
+    summary_lines = ''.join(['x','dneg','dpos'])
+    details_lines = ''.join(['x','s','dneg','dpos'])
+    for x_id, result in results.items():
+        x = int(x_id[1:])
+        summary, details = result
+        summary_lines.append(','.join([x, summary[0], summary[1]]))
+        details_lines.extend([','.join([x, s, detail[0], detail[1]]) for s,detail in enumerate(details)])
+    summary_file = os.path.join(outdir, f'summary_{outid}.csv' if outid else 'summary.csv')
+    details_file = os.path.join(outdir, f'details_{outid}.csv' if outid else 'details.csv')
+    if not os.path.exists(outdir):
+        os.mkdirs(outdir)
+    with open(summary_file, 'w') as f:
+        f.writelines(summary_lines)
+        logger(f'wrote summary to {summary_file}')
+    with open(details_file, 'w') as f:
+        f.writelines(details_lines)
+        logger(f'wrote detils to {details_file}')
 
 def find_feature_sensitivity_boundaries(model_path, x, samples, d_min=0.001, d_max=100, multithread=False, verbose=0):
     '''
@@ -30,6 +65,7 @@ def find_feature_sensitivity_boundaries(model_path, x, samples, d_min=0.001, d_m
     @d_max (float): max distance to consider (default false)
     @multithread (bool): perform + and - in parallel (default false)
     @verbose (int): extra logging (0, 1, 2) (default 0)
+    @return (tuple): ((negdist,posdist), [(s0_ndist,s0_pdist)...(sN_ndist,sN_pdist)])
     '''
     def find_distance(s, sign):
         inputs, outputs = samples[s]
@@ -78,7 +114,7 @@ def find_feature_sensitivity_boundaries(model_path, x, samples, d_min=0.001, d_m
 
     return ((negd, posd), results)
 
-def find_sensitivity_boundaries(model_path, samples, d_min=0.001, d_max=100, multithread=False, verbose=0):
+def find_sensitivity_boundaries(model_path, samples, d_min=0.001, d_max=100, multithread=False, verbose=0, save_results=False):
     '''
     finds sensitivity for all features relative to the provided samples
 
@@ -88,11 +124,11 @@ def find_sensitivity_boundaries(model_path, samples, d_min=0.001, d_max=100, mul
     @d_max (float): max distance to consider
     @multithread (bool): perform + and - in parallel (default false)
     @verbose (int): extra logging (0, 1, 2) (default 0)
+    @return (dict): {x0:((negdist,posdist), [(s0_ndist,s0_pdist)...(sN_ndist,sN_pdist)]),...}
     '''
     n_features = len(samples[0][0])
     results = {}
     for x in range(n_features):
-        result = find_feature_sensitivity_boundaries(model_path, x, samples, d_min=d_min, d_max=d_max, multithread=multithread, verbose=verbose)
-        results[f'x{x}'] = result
+        results[f'x{x}'] = find_feature_sensitivity_boundaries(model_path, x, samples, d_min=d_min, d_max=d_max, multithread=multithread, verbose=verbose)
+    if save_results: save_sensitivity_results_to_csv(results)
     return results
-    
