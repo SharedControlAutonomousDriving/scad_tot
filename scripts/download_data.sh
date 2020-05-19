9c794d64f2d23166c8580d85a66fe1b9af137639
John Grese <johngrese@yahoo.com>
2020-05-18 22:29:08 -0700

    added data download

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -2,11 +2,18 @@
 
 # v3.2.0_train: https://drive.google.com/file/d/1GF9I0XnhztkYR1ZPNcsc6Mdq-xg2MP93/view?usp=sharing
 # v3.2.0_test: https://drive.google.com/file/d/1KkoEaLUsmV916TO9FOAuGmN_uy2o9e-t/view?usp=sharing
+# public folder: https://drive.google.com/drive/folders/1ZWyP6BiUhch94XKGMDs2B1L2T-KUwS_I
 
-DATA_PATH=/home/marabou/scad_tot/data
-TRAIN_DATA_URL=https://drive.google.com/file/d/1GF9I0XnhztkYR1ZPNcsc6Mdq-xg2MP93
-TEST_DATA_URL=https://drive.google.com/file/d/1KkoEaLUsmV916TO9FOAuGmN_uy2o9e-t
+DATA_PATH=./data
+
+BASE_DL_URL="https://drive.google.com/uc?authuser=0&export=download"
+TEST_DATA_URL="$BASE_DL_URL&id=1KkoEaLUsmV916TO9FOAuGmN_uy2o9e-t"
+TRAIN_DATA_URL="$BASE_DL_URL&id=1GF9I0XnhztkYR1ZPNcsc6Mdq-xg2MP93"
 
 mkdir -p $DATA_PATH
-wget -O $DATA_PATH/data/test.csv $TEST_DATA_URL
-wget -O $DATA_PATH/data/train.csv $TRAIN_DATA_URL
+
+# uncomment to download both training and test data.
+# wget -O $DATA_PATH/train.csv $TRAIN_DATA_URL
+# wget -O $DATA_PATH/test.csv $TEST_DATA_URL
+
+wget -O $DATA_PATH/data.csv $TEST_DATA_URL
--- scripts/robustness.sh
+++ scripts/robustness.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-python3 verification/robustness.py -m ./network/models/v3.2.0/model.h5 -d ./data/v3.2.0/test.csv -df 0.01 -dmin 0.00001 -dmax 100 -mt -sr -ss -sl -v 1
+python3 verification/robustness.py -m ./network/models/v3.2.0/model.h5 -d ./data/data.csv -df 0.01 -dmin 0.00001 -dmax 100 -mt -sr -ss -sl -v 1
