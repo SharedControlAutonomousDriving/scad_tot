680be13f3f2584a3f5ab71df3a85e9e89c79f060
John Grese <johngrese@yahoo.com>
2020-05-27 08:42:04 -0700

    general cleanup

--- verification/utils.py
+++ verification/utils.py
@@ -138,9 +138,14 @@ class TOTUtils:
         '''
         if not os.path.exists(outdir):
             os.makedirs(outdir, mode=0o755)
+        outfile = os.path.join(outdir, 'samples.csv')
         df = pd.concat([
             pd.DataFrame([s[0] for s in samples], columns=TOTUtils._features),
             pd.DataFrame([s[1] for s in samples], columns=[f'TOT_{c}' for c in TOTUtils._categories])
             ], axis=1)
-        df.to_csv(os.path.join(outdir, 'samples.csv'))
-        
+        df.to_csv(outfile)
+        return outfile
+    
+    @staticmethod
+    def build_adversarial_samples(self, target_samples, outdir):
+        pass
