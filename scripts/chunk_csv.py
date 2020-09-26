6c00dbce8126d9729e28424fc810b6eef543db60
John Grese <johngrese@yahoo.com>
2020-09-25 23:49:02 -0700

    updated scripts
    - updated chunk & combine csv scripts
    - added comments and documentation to docker_run

--- scripts/chunk_csv.py
+++ scripts/chunk_csv.py
@@ -1,5 +1,21 @@
 #!venv/bin/python3
 
+"""
+chunk_csv.py
+
+Splits a CSV into chunks and handles the headers
+
+Examples:
+help
+./scripts/chunk_csv.py -h
+
+split input.csv (with header row) into chunks of 100 rows
+./scripts/chunk_csv.py -f input.csv -s 100 -o ./outdir
+
+split input.csv (without header row) into chunks of 10 rows
+./scripts/chunk_csv.py -f input.csv -s 100 -nh -o ./outdir
+"""
+
 import sys, os
 from argparse import ArgumentParser
 
@@ -23,10 +39,9 @@ def chunk_csv(csv_path, chunk_size, outdir='./', with_header=True):
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('-f', '--csvfile', required=True)
-    parser.add_argument('-s', '--chunksize', type=int, required=True)
-    parser.add_argument('-nh', '--noheader', action='store_true')
-    parser.add_argument('-o', '--outdir', default='./')
+    parser.add_argument('-f', '--csvfile', required=True, help='input csv file')
+    parser.add_argument('-s', '--chunksize', type=int, required=True, help='size of output csv chunks')
+    parser.add_argument('-nh', '--noheader', action='store_true', help='use if input csv does not have header row')
+    parser.add_argument('-o', '--outdir', default='./', help='output directory to chunked csv files to')
     args = parser.parse_args()
     chunk_csv(args.csvfile, args.chunksize, outdir=args.outdir, with_header=not args.noheader)
-
--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
+# Run the docker image locally
 docker run -p 9999:9999 -e SERVER_IP=127.0.0.1 -e grese/scad_tot
