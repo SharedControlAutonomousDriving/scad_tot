8e48d741e1a7cf0bb613f5f620611307d65c802c
Aiswarya Vinod Kumar <37078306+aiskumo@users.noreply.github.com>
2020-12-16 20:20:51 -0500

    Update adversarial_training.py

--- adv/code/adversarial_training.py
+++ adv/code/adversarial_training.py
@@ -44,7 +44,7 @@ def train_step(model, data, labels):
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     
 
-def save_model1(adv_model, exp):
+def save_adv_model(adv_model, exp):
     # path = os.path.join(os.getcwd(),'models/')
     path = '/content/drive/MyDrive/SCAD/models/'
 
@@ -53,7 +53,7 @@ def save_model1(adv_model, exp):
     except:
         print("Folder may already exist.")
 
-    filename = path + 'adv_model{}.h5'.format(exp)  
+    filename = path + 'adv_model{}.h5'.format(exp)
     adv_model.model.save(filename)
 
 
@@ -82,7 +82,8 @@ if __name__ == "__main__":
     logger.info("Experiment : {}".format(exp))
     logger.info("Epsilon:{}".format(eps))
     logger.info("="*50)
-
+ 
+    
     print("Loading model...\n")
     new_model = tf.keras.models.load_model("/content/drive/MyDrive/SCAD/network/models/latest/model.h5")
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
@@ -104,10 +105,10 @@ if __name__ == "__main__":
     _, orig_acc = new_model.evaluate(test_data, test_labels, verbose=1)
     logger.info("Classifier with original training")
     logger.info("Accuracy on clean test samples: %.2f%%", (orig_acc * 100))
+    logger.info("="*50)
     
 
     print("Creating classifier...\n")
-    
     adv_classifier = TensorFlowV2Classifier(
         model=new_model,
         loss_object=loss_object,
@@ -119,26 +120,33 @@ if __name__ == "__main__":
     
     
     print("Creating adversarial attack object...\n")
-    fgsm = FastGradientMethod(adv_classifier, norm=np.inf, eps=eps, eps_step=0.001, targeted=False, batch_size=2048, num_random_init=27)
+    fgsm = FastGradientMethod(adv_classifier, 
+                              norm=np.inf, 
+                              eps=eps, 
+                              eps_step=0.001, 
+                              targeted=False, 
+                              batch_size=2048, 
+                              num_random_init=27)
 
-    
+
     print("Generating adversarial samples...\n")
     logger.info("Craft attack on training examples")
     x_train_adv = fgsm.generate(train_data)
     save_samples(x_train_adv, 'adv_train', exp)
 
-    
     logger.info("Craft attack test examples")
     x_test_adv = fgsm.generate(test_data)
     save_samples(x_test_adv, 'adv_test', exp)
-      
+    
 
-    print("Evaluating adversarial samples before training...\n")
+    print("Evaluating adversarial samples on clean model...\n")
     preds = np.argmax(adv_classifier.predict(x_test_adv), axis=1)
     acc = np.sum(preds == non_encoded_test_labels) / non_encoded_test_labels.shape[0]
     logger.info("Classifier before adversarial training")
     logger.info("Accuracy on adversarial samples: %.2f%%", (acc * 100))
+    logger.info("="*50)
 
+    
     
     print("Augmenting original data with adversarial samples...\n")
     aug_train_data = np.append(train_data, x_train_adv, axis=0)
@@ -148,20 +156,22 @@ if __name__ == "__main__":
     print("Retraining model on new dataset...\n")
     new_model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
     adv_classifier.fit(aug_train_data, aug_train_labels, nb_epochs=10, batch_size=2048)
-    save_model1(adv_classifier, exp)
-
+    save_adv_model(adv_classifier, exp)
+    print("Saved model...\n")
     
     print("Evaluate adversarially trained model on adversarial test samples...\n")
     preds_adv = np.argmax(adv_classifier.predict(x_test_adv), axis=1)
-    acc_adv = np.sum(preds == non_encoded_test_labels) / non_encoded_test_labels.shape[0]
+    acc_adv = np.sum(preds_adv == non_encoded_test_labels) / non_encoded_test_labels.shape[0]
     logger.info("Classifier with adversarial training")
-    logger.info("Accuracy on adversarial test samples: %.2f%%", (acc * 100))
+    logger.info("Accuracy on adversarial test samples: %.2f%%", (acc_adv * 100))
+    logger.info("="*50)
 
     print("Evaluate adversarially trained model on clean test samples...\n")
-    preds = np.argmax(adv_classifier.predict(test_data), axis=1)
-    acc = np.sum(preds == non_encoded_test_labels) / non_encoded_test_labels.shape[0]
+    new_preds_clean = np.argmax(adv_classifier.predict(test_data), axis=1)
+    new_acc_clean = np.sum(new_preds_clean == non_encoded_test_labels) / non_encoded_test_labels.shape[0]
     logger.info("Classifier with adversarial training")
-    logger.info("Accuracy on clean test samples: %.2f%%", (acc * 100))
+    logger.info("Accuracy on clean test samples: %.2f%%", (new_acc_clean * 100))
+    logger.info("="*50)
     
 
     
