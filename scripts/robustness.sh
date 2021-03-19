4084a4b8b623e35d8d04ae076d09f238f53836e4
John Grese <johngrese@yahoo.com>
2021-03-19 03:22:01 -0700

    deleted robustness_asym.sh

--- scripts/robustness.sh
+++ scripts/robustness.sh
@@ -2,3 +2,4 @@
 
 # run symmetric local robustness test
 python3 verification/robustness.py -n ./network/models/v3.2.0/model.nnet -d ./data/v3.2.0/test.csv -df 0.01 -emin 0.00001 -emax 100 -eprec 0.000001 -sr -ss -sl -v 1 -o ./logs/robustness/symmetric -ck
+
