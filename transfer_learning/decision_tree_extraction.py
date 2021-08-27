f1d034a5ed3adc3caa8477f68f45b70f6ae05187
ravi <ravi9788@gmail.com>
2021-08-27 11:40:14 -0700

    print marabou queries from decision tree rules (bug fixes)

--- transfer_learning/decision_tree_extraction.py
+++ transfer_learning/decision_tree_extraction.py
@@ -89,7 +89,7 @@ def print_marabou_query(path, lbs, ubs, file_path):
         with open(fpath, 'w') as f1:
             for j in range(0, lbs.size):
                 f1.write(f'x{j} >= {lbs[j]}\n')
-                f1.write(f'x{j} < {ubs[j]}\n')
+                f1.write(f'x{j} <= {ubs[j]}\n')
             f1.write(f'y{incorrect_labels[i]} -y{correct_label} >= 0')
 
 
